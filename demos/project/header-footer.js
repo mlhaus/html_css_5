bdafdc24c5a025e6c3338f3c67c240c9c00fa887
Marc Hauschildt <marc.hauschildt@gmail.com>
2024-11-05 08:58:49 -0600

    Oct 31

--- demos/project/header-footer.js
+++ demos/project/header-footer.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
-    $("body > header").load("header.html");
+    var currentFileArr = document.location.href.match(/[^\/]+$/);
+    var currentFile = currentFileArr != null ? currentFileArr[0] : "index.html";
+    $("body > header").load("header.html", function () {
+        $('header nav a').each(function() {
+            // Check if the link's href matches the current URL
+            if (currentFile.indexOf($(this).attr('href')) >= 0) {
+                // Add the 'active' class to the matching link
+                $(this).addClass('current-page');
+            }
+        });
+    });
     $("body > footer").load("footer.html");
 });
